d7ef02bfc31325dff6cd0fb54d44726ddb9f96a3
Leah Schlackman <leahemily1@gmail.com>
2020-09-15 17:54:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog 
 
   def name=(dog_name)
-    @this_dogs_name = dog_name
+    @name = dog_name
   end
   
   def name
-    @this_dogs_name
+    @name
   end
   
   def breed=(dog_breed)
@@ -15,8 +15,5 @@ class Dog
   def breed 
     @this_dogs_breed 
   end
-  
-
-
 
 end 
